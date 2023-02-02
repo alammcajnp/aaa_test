e44da20099c7c70164f5fefae33c6ab8115b0895
alammcajnp <alamjnp12@gmail.com>
2023-02-02 22:48:10 +0530

    add

--- src/modules/customer/customer.controller.ts
+++ src/modules/customer/customer.controller.ts
@@ -21,7 +21,7 @@ export class CustomerController {
   // Retrieve customers list
   @Get('/get')
   async get_data(@Res() res) {
-    return res.status(HttpStatus.OK).json('Testing 123456');
+    return res.status(HttpStatus.OK).json('Testing 1 2 3');
   }
 
   // add a customer
